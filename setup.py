34657a7f4fc30a463b232706f148d26eab20b3e1
Monstradamus <bohdan.y@starnavi.io>
2024-02-09 13:20:59 +0200

    Update setup.py with aditional parameters for setup

--- setup.py
+++ setup.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
 from setuptools import find_packages, setup, Command
 
 
@@ -21,7 +23,7 @@ class TestCommand(Command):
                     'NAME': 'partitioning',
                     'USER': 'postgres',
                     'PASSWORD': 'postgres',
-                    'HOST': '127.0.0.1',
+                    'HOST': 'localhost',
                     'PORT': '5432',
                 }
             },
@@ -31,17 +33,28 @@ class TestCommand(Command):
         call_command('test', 'tests')
 
 
+with open('README.md', 'r', encoding='utf-8') as f:
+    long_description = f.read()
+
+
 setup(
     name='django-partitioning',
     version='0.1',
+    # url='https://github.com/starnavi-team/django-postgres-partitioning',
+    url='https://github.com/BohdanYatsyna/django-postgres-partitioning',
     packages=find_packages(),
     include_package_data=True,
     license='BSD License',
     description='Add partitioning to django models',
-    long_description=open('README.md').read(),
-    author='Soin Sergey',
-    author_email='soins1992@gmail.com',
-    install_requires=['Django >= 1.11', 'setuptools'],
-    tests_require=['Django >= 1.11', 'setuptools'],
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    author='starnavi.io',
+    author_email='hello@starnavi.io',
+    install_requires=[
+        'Django>=1.11,<5.1', 'psycopg2-binary>=2.7.5,<3.0', 'setuptools'
+    ],
+    tests_require=[
+        'Django>=1.11,<5.1', 'psycopg2-binary>=2.7.5,<3.0', 'setuptools'
+    ],
     cmdclass={'test': TestCommand},
 )
