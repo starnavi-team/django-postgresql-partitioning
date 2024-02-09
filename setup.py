e6974dac9d793895496fd47ed75bcc57db8f337f
Monstradamus <bohdan.y@starnavi.io>
2024-02-09 15:20:59 +0200

    Update version to 0.1.1 for Test PyPI publishing

--- setup.py
+++ setup.py
@@ -39,7 +39,7 @@ with open('README.md', 'r', encoding='utf-8') as f:
 
 setup(
     name='django-partitioning',
-    version='0.1',
+    version='0.1.1',
     # url='https://github.com/starnavi-team/django-postgres-partitioning',
     url='https://github.com/BohdanYatsyna/django-postgres-partitioning',
     packages=find_packages(),
@@ -57,4 +57,20 @@ setup(
         'Django>=1.11,<5.1', 'setuptools'
     ],
     cmdclass={'test': TestCommand},
+    classifiers=[
+        'Development Status :: 2 - Pre-Alpha',
+        'Framework :: Django',
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Natural Language :: English',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
+        'Programming Language :: Python :: 3.11',
+        'Programming Language :: Python :: 3.12',
+    ]
 )
