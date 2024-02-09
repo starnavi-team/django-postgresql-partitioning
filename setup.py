da33fb3f4e1a0fb54c20a5668830e58edada5c8f
Monstradamus <bohdan.y@starnavi.io>
2024-02-09 16:11:32 +0200

    Replace find_packages to find_namespace_packages in setup.py

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-from setuptools import find_packages, setup, Command
+from setuptools import setup, Command, find_namespace_packages
 
 
 class TestCommand(Command):
@@ -39,10 +39,10 @@ with open('README.md', 'r', encoding='utf-8') as f:
 
 setup(
     name='django-partitioning-postgres',
-    version='0.1',
+    version='0.2',
     # url='https://github.com/starnavi-team/django-postgres-partitioning',
     url='https://github.com/BohdanYatsyna/django-postgres-partitioning',
-    packages=find_packages(),
+    packages=find_namespace_packages(),
     include_package_data=True,
     license='BSD License',
     description='Add PostgreSQL table partitioning to Django models',
